628de72d8c27a895cbbaec6cfae20050e92930c3
Jackson Ramalho <jackson.ramalho@rumosolucoes.com.br>
2020-06-30 13:31:50 -0300

    Tabela Simples

--- src/app/Main/tabela-dados-agrupados/tabela-dados-agrupados.component.ts
+++ src/app/Main/tabela-dados-agrupados/tabela-dados-agrupados.component.ts
@@ -10,7 +10,7 @@ export interface TabelaDadosAgrupados {
   fi:number[];
   Fi:number[];
   fr:number[];
-  Fr:number[];
+  Fr:number[];  
 }
 export interface DadosTabelaDadosAgrupados {
   Amplitude:number;
@@ -39,19 +39,31 @@ export class TabelaDadosAgrupadosComponent implements OnInit {
   @ViewChild(MatTable) MatTable: MatTable<any>;
   tabela:TabelaDadosAgrupados[] = [];
   DadosTabela:DadosTabelaDadosAgrupados = null;
-  NomeTabela:string = "Intervalos";
+  NomeTabela:string = "Dados";
   valoresTabela:string;
+  Simples:boolean;
   GerarTabela(dados :string){
     this.valoresTabela = dados;
     this.restApi.getTabelaDadosAgrupados(dados).subscribe(data => {
       data = JSON.parse(data);
       this.tabela = [];
       this.DadosTabela = null;
-
+      this.Simples=data.Simples
+      console.log(data);
       for (let i = 0; i < data.QuantidadeIntervalos; i++)
       {
-          this.tabela.push(
-            {
+        if(data.Simples){
+          this.tabela.push({
+            intervalos:data.ValoresDistintos[i].Key,
+            xi: [0],
+            fi:data.ValoresDistintos[i].Value,
+            Fi:data.Fi[i],
+            fr:data.fr[i],
+            Fr:data.Fr[i],
+          });
+        }
+        else{
+            this.tabela.push({
               intervalos:data.intervalos[i],
               xi:data.xi[i],
               fi:data.fi[i],
@@ -60,24 +72,26 @@ export class TabelaDadosAgrupadosComponent implements OnInit {
               Fr:data.Fr[i],
             });
         }
-        this.DadosTabela = {
-            Amplitude:data.Amplitude,
-            NumeroDeElementos:data.NumeroDeElementos,
-            QuantidadeIntervalos:data.QuantidadeIntervalos,
-            Intervalo:data.Intervalo,
-            ValorMinimo:data.ValorMinimo,
-            ValorMaximo:data.ValorMaximo,
-            Moda:data.Moda,
-            Passos:data.Passos,
-            Mediana:data.Mediana,
-            Media:data.Media,
-            DesvioPadrao:data.DesvioPadrao,
-            Variancia:data.Variancia
-        };
-        localStorage.setItem('last_table_values', JSON.stringify(this.valoresTabela));
-        localStorage.setItem('last_table_data', JSON.stringify(this.DadosTabela));
-        localStorage.setItem('last_table_search', JSON.stringify(this.tabela));
-        this.MatTable.renderRows();
+      }
+      this.DadosTabela = 
+      {
+        Amplitude:data.Amplitude,
+        NumeroDeElementos:data.NumeroDeElementos,
+        QuantidadeIntervalos:data.QuantidadeIntervalos,
+        Intervalo:data.Intervalo,
+        ValorMinimo:data.ValorMinimo,
+        ValorMaximo:data.ValorMaximo,
+        Moda:data.Moda,
+        Passos:data.Passos,
+        Mediana:data.Mediana,
+        Media:data.Media,
+        DesvioPadrao:data.DesvioPadrao,
+        Variancia:data.Variancia,
+      };
+      localStorage.setItem('last_table_values', JSON.stringify(this.valoresTabela));
+      localStorage.setItem('last_table_data', JSON.stringify(this.DadosTabela));
+      localStorage.setItem('last_table_search', JSON.stringify(this.tabela));
+      this.MatTable.renderRows();
     });
   }
 
@@ -94,11 +108,11 @@ export class TabelaDadosAgrupadosComponent implements OnInit {
   displayedColumns: string[] = ['intervalos', 'xi', 'fi', 'Fi','fr','Fr'];
 
   ngOnInit(): void {
-    if(localStorage.getItem('last_table_search') !== null){
-      this.tabela = JSON.parse(localStorage.getItem('last_table_search'));
-      this.valoresTabela = JSON.parse(localStorage.getItem('last_table_values'));
-      this.DadosTabela = JSON.parse(localStorage.getItem('last_table_data'));
-    }
+    // if(localStorage.getItem('last_table_search') !== null){
+    //   this.tabela = JSON.parse(localStorage.getItem('last_table_search'));
+    //   this.valoresTabela = JSON.parse(localStorage.getItem('last_table_values'));
+    //   this.DadosTabela = JSON.parse(localStorage.getItem('last_table_data'));
+    // }
   }
 
 }
